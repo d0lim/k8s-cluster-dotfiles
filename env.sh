5f566ef0aa2d64610b0cc0c1d930a996a001e792
D0Lim <limdongyoung0@gmail.com>
2023-11-23 22:51:58 +0900

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,3 +3,5 @@ export EMAIL="your.email@example.com"
 export VPN_IP="172.123.3.4"
 export ROUTER_IP="172.123.5.6"
 export ROUTER_GATEWAY_IP="192.168.0.10"
+export REGISTRY_USERNAME="username"
+export REGISTRY_PASSWORD='1234'
